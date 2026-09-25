8e1a9f79689f3813224358c0ff50bde7754f27ab
AMAN40011 <amanpal142005@gmail.com>
2026-09-26 03:26:38 +0530

    Initial commit - Ideamagix lecture scheduling module

--- Server/index.js
+++ Server/index.js
@@ -14,12 +14,10 @@ const app = express();
 
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
